520fe89d738128c7654ff2cf65b8472b8b33a81a
Musa <musaajmal56@gmail.com>
2025-12-18 11:31:32 +0500

    take 45

--- backend/database.py
+++ backend/database.py
@@ -6,14 +6,9 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "postgres")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+DATABASE_URL ="postgresql://avnadmin:AVNS_OQ0K1gICnzak5iV_ukC@pg-35318557-cuilahore-63ed.j.aivencloud.com:19969/defaultdb?sslmode=require"
 # if not DATABASE_URL:
 #     raise ValueError("No DATABASE_URL set in environment variables")
 
